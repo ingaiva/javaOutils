812034e99e9cf229321150022c7c7b16d0fb5848
Inga <ingaiva@msn.com>
2020-07-29 16:25:59 +0200

    Ajout des methodes IO

--- Outils.java
+++ Outils.java
@@ -1,8 +1,15 @@
 package javaOutils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 public class Outils {
 
@@ -33,5 +40,57 @@ public class Outils {
 
 	static ArrayList<Integer> toArray(Integer[] tabInt) {
 		return new ArrayList<Integer>(Arrays.asList(tabInt));
+	}
+
+	static void listerEnvVar() {
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+			System.out.format("%s=%s%n", envName, env.get(envName));
+		}
+	}
+
+	static String getDeskTopPath() {
+//	String oneDrivePath=System.getenv("OneDrive");
+		File dirRoot = new File(
+				System.getProperty("user.home") + File.separator + "OneDrive" + File.separator + "Bureau");
+		return dirRoot.getPath();
+	}
+
+	static void readFileToConsole(File monFile) {
+		try {
+			if (monFile.exists()) {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(new FileInputStream(monFile), "UTF-8"));
+				String line;
+				do {
+					line = reader.readLine();
+					if (line != null)
+						System.out.println(line);
+				} while (line != null);
+				reader.close();
+			} else
+				System.out.println("Le fichier  '" + monFile.getPath() + "' n'existe pas");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	static void writeToFile(File monFile, ArrayList<String> data) {
+		try {
+			if (!monFile.exists())
+				monFile.createNewFile();
+
+			FileWriter writer = new FileWriter(monFile);
+			BufferedWriter bwriter = new BufferedWriter(writer);
+
+			for (String elt : data) {
+				bwriter.write(elt);
+				bwriter.newLine();
+			}
+			bwriter.close();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
